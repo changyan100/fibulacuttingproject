24107af54de1b7a6554a0644f46b887db937146d
changyan <changyanhe@126.com>
2021-01-27 18:59:02 +0800

    add robot adjoint matrix

--- src/script/robotcontrol.py
+++ src/script/robotcontrol.py
@@ -15,6 +15,7 @@ import rtde_control
 import rtde_receive
 
 from std_msgs.msg import Bool
+from scipy.spatial.transform import Rotation as R
 
 
 
@@ -53,7 +54,11 @@ class ImpendancControl():
 		for x in range(0,6):
 			force_rebiased[x] = self.atiforce[x] - self.rebias[x]
 			print("%.3f" % force_rebiased[x])
+		
+		# tuning admittance scale here
 		scale = [0.01, 0.01,0.01, 0.01,0.01, 0.01]
+		
+
 		vel = [0,0,0,0,0,0]
 		for x in range(0,3):
 			if force_rebiased[x] > 1: #set a active threshold for force xyz
@@ -62,12 +67,70 @@ class ImpendancControl():
 			if force_rebiased[x] > 0.02: #set a active threshold for torque xyz
 				vel[x] = scale[x]*force_rebiased[x]
 
+		FwdKin = rtde_c.getForwardKinematics()
+		pose = np.zeros([3])
+		pose[0] =  FwdKin[3]
+		pose[1] =  FwdKin[4]
+		pose[2] =  FwdKin[5]
+		r = R.from_rotvec(pose)
+		trans = np.zeros([3])
+		trans[0] =  FwdKin[0]
+		trans[1] =  FwdKin[1]
+		trans[2] =  FwdKin[2]
+
+		outVel = self.VelAdjointTrans(r, trans, vel)
+		
+
 		if self.robotmoveflag == True:
-			print("robot vel:", vel)
-			self.rtde_c.speedL( vel,0.2)
+			print("robot vel:", outVel)
+			self.rtde_c.speedL( outVel,0.2)
 		else:
 			self.rtde_c.stopL(8)
 			# self.rtde_c.stopScript()
+
+	def VelAdjointTrans(self, inRot, inTrans, inVel):
+		px = inTrans[0]
+		py = inTrans[1]
+		pz = inTrans[2]
+		Skew_p = np.zeros([3,3])
+		Skew_p[0,0] = 0
+		Skew_p[0,1] = -pz
+		Skew_p[0,2] = py
+		Skew_p[1,0] = pz
+		Skew_p[1,1] = 0
+		Skew_p[1,2] = -px
+		Skew_p[2,0] = -py
+		Skew_p[2,1] = px
+		Skew_p[2,2] = 0
+		
+		AdTrans = np.zeros([6,6])
+
+		for i in range(0,3):
+			for j in range(0,3):
+				AdTrans[i,j] = inRot[i,j]
+				j = j+1
+			i = i+1
+
+		for i in range(3,6):
+			for j in range(3,6):
+				AdTrans[i,j] = inRot[i,j]
+				j = j+1
+			i = i+1
+
+		Skew_p_r = np.dot(Skew_p, inRot)
+
+		for i in range(0,3):
+			for j in range(3,6):
+				AdTrans[i,j] = Skew_p_r[i,j]
+				j = j+1
+			i = i+1
+
+
+		outVel = np.dot(AdTrans, inVel.transpose())
+
+		return outVel
+
+
 
 
 	def callback_atirebias(self, data):
